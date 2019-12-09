9a9349988f04dac9d666075b3b5d23599eafefd8
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-12-09 21:07:39 +0000

    input answers

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Script that creates a file in /tmp using Puppet
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
