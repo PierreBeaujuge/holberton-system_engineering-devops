e3b040401d8b65c9270088427d0e5e4cc4842aae
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-02-06 20:09:19 +0000

    reordered libraries

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,10 +4,10 @@ Write a Python script that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0
 """
-import requests
-import sys
 import json
 # import pprint
+import requests
+import sys
 
 headers = {
     'User-Agent': 'My User Agent 1.0'
